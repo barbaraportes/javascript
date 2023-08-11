51f8e2217967c27fe70e7f5d29f8510db9b47206
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-11 09:34:37 -0300

    ex condicao simples if

---
+++ aula11/ex001.js
@@ -0,0 +1,10 @@
+var vel = 10
+console.log(`A velocidade do seu carro é ${vel}Km/hr`)
+
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+
