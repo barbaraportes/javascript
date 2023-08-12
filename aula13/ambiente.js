a9ccad5652303856c2c01a57d4fd79330109703c
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-12 13:54:43 -0300

    estruturas de repeticao
    com teste no inicio ( while)
    com teste no final (do ...while)

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+
+var c = 1
+
+// while (c <= 50) { // estrutura de repetição com teste no inicio
+//     console.log(`${c}º Tudo bem?`)
+//     c++ // c = c + 1
+// }
+
+do { //estrutura de repetição com teste no final
+    console.log(`${c}º Tudo bem?`)
+    c++
+} while (c <=10)
