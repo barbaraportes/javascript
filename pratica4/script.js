144ecbf2fc943bb0551ab1e67110971c97639293
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-12 20:28:00 -0300

    tabuada

--- pratica4/script.js
+++ pratica4/script.js
@@ -1,1 +1,24 @@
-alert('Ola, Mundo')
+function gerar() {
+
+    let num = document.querySelector('#numero')
+    let tab = document.querySelector('#seltab')
+
+    if (num.value.length == 0) {
+        alert('[ERRO] Digite um valor.')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+
+        let c = 1
+
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
+
