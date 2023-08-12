abc7b9fdf36c80dd083d08f7421e1a3b61a82d5a
Barbara Portes <134977381+barbaraportes@users.noreply.github.com>
2023-08-12 15:05:00 -0300

    repeticao com controle
    for

---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+
+// var c = 1
+// while (c <= 10) {
+//     console.log(c)
+//     c++
+// }
+
+console.log('Vai começar...')
+for (var c=1; c<=4; c++) {
+    console.log(c)
+}
+console.log('Fim.')
+
